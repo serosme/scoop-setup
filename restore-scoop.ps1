b032702f332efec92fb6fd651e9ae97b7d4c608d
seros <github@seros.me>
2026-01-24 16:35:32 +0800

    feat: 更新

--- restore-scoop.ps1
+++ restore-scoop.ps1
@@ -1,10 +1,32 @@
 $ErrorActionPreference = "Stop"
 
-$BackupUrl = "https://script.seros.me/scoop"
-$TempFile  = Join-Path $env:TEMP "scoop-backup.json"
+$BaseUrl = "https://raw.githubusercontent.com/serosme/scoop-setup/main/scoop"
+$TempFile = Join-Path $env:TEMP "scoop-backup.json"
+
+$Backups = @(
+    "scoop-base.json",
+    "scoop-dev.json"
+)
 
 try {
-    Write-Host "Downloading scoop backup..."
+    Write-Host "Available scoop backups:`n"
+
+    for ($i = 0; $i -lt $Backups.Count; $i++) {
+        Write-Host "[$($i + 1)] $($Backups[$i])"
+    }
+
+    $choice = Read-Host "`nSelect a backup to import (1-$($Backups.Count))"
+
+    if (-not ($choice -as [int]) -or
+        $choice -lt 1 -or
+        $choice -gt $Backups.Count) {
+        throw "Invalid selection."
+    }
+
+    $SelectedBackup = $Backups[$choice - 1]
+    $BackupUrl = "$BaseUrl/$SelectedBackup"
+
+    Write-Host "`nDownloading $SelectedBackup..."
     Invoke-WebRequest $BackupUrl -OutFile $TempFile
 
     Write-Host "Importing scoop backup..."
